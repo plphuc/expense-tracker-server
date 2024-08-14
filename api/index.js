77c9eb725dba5b03ccfb5871c45506d87cbad5f1
plphuc <phamluumyphuc@gmail.com>
2024-08-15 04:01:54 +0700

    init

--- api/index.js
+++ api/index.js
@@ -1,31 +1,33 @@
-// npm install @apollo/server express graphql cors
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import passport from 'passport';
 import session from 'express-session';
 import connectMongo from 'connect-mongodb-session';
-import passport from 'passport';
-import { configurePassport } from './passport/passport.config.js';
 
-import { ApolloServer } from 'apollo-server-express';
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+
+import { buildContext } from 'graphql-passport';
+
+import mergedResolvers from './resolvers/index.js';
+import mergedTypeDefs from './typeDefs/index.js';
 
 import { connectDB } from './db/connectDB.js';
-import mergedTypeDefs from './typeDefs/index.js';
-import mergedResolvers from './resolvers/index.js';
+import { configurePassport } from './passport/passport.config.js';
+import httpStatus from 'http-status';
 
 dotenv.config();
 configurePassport();
 
-// Required logic for integrating with Express
-const app = express();
+const app = new express();
 
-// Our httpServer handles incoming requests to our Express app.
-// Below, we tell Apollo Server to "drain" this httpServer,
-// enabling our servers to shut down gracefully.
+const httpServer = http.createServer(app);
 
 const MongoDBStore = connectMongo(session);
+
 const store = new MongoDBStore({
     uri: process.env.MONGO_URI,
     collection: 'sessions'
@@ -37,10 +39,12 @@ app.use(
     session({
         secret: process.env.SESSION_SECRET,
         resave: false, // this option specifies whether to save the session to the store on every request
-        saveUninitialized: false, //this option specifies whether to save uninitialized sessions
+        saveUninitialized: false, // option specifies whether to save uninitialized sessions
         cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 7,
-            httpOnly: true // this option prevents the Cross-site scripting (XSS) attacks
+            // httpOnly: true, // this option prevents the Cross-Site Scripting (XSS) attacks
+			sameSite: 'none', 
+			secure: true
         },
         store: store
     })
@@ -49,32 +53,35 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(
-    '/graphql',
-    cors({
-        credentials: true // to use cookie
-    }),
-    express.json()
-);
-
-// Same ApolloServer initialization as before, plus the drain plugin
-// for our httpServer.
-const httpServer = http.createServer(app);
-
 const server = new ApolloServer({
     typeDefs: mergedTypeDefs,
     resolvers: mergedResolvers,
-    persistedQueries: false,
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
 });
 
+// Ensure we wait for our server to start
 await server.start();
 
 // Set up our Express middleware to handle CORS, body parsing,
 // and our expressMiddleware function.
+app.use(
+    '/graphql',
+    cors({
+        origin: true,
+        credentials: true
+    }),
+    express.json(),
+    // expressMiddleware accepts the same arguments:
+    // an Apollo Server instance and optional configuration options
+    expressMiddleware(server, {
+        context: async ({ req, res }) => buildContext({ req, res })
+    })
+);
 
-server.applyMiddleware({ app });
+app.use('*', (req, res) =>
+    res.status(httpStatus.OK).send({ message: 'Fuck GraphQL' })
+);
 
+// Modified server startup
 await connectDB();
-
 export default httpServer;
--- dev.js
+++ dev.js
@@ -1,6 +1,4 @@
-import dotenv from 'dotenv';
 import app from './api/index.js'
 
-dotenv.config()
-
-app.listen(process.env.port || 4000, () => console.log("Server started ", app.address().port));
+app.listen(4000, () => {console.log("Server started");
+})
